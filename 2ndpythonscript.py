cf2b8fb775d7a0792f610292f25486b365557d82
JessicaGarson <jessica.garson@gmail.com>
2015-11-05 16:08:50 -0500

    commiting scripts

---
+++ 2ndpythonscript.py
@@ -0,0 +1,9 @@
+name_of_student = "Jess"
+print name_of_student
+
+statement = "Hello {0}, it's nice to meet you".format(name_of_student)
+print statement
+
+color = raw_input("Whats your favorite color? ")
+response = "Well {0}, {1} is a great color, nice to meet you, have a great day".format(name_of_student, color)
+print response
